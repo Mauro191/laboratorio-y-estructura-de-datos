13cb906dcc5b5b105d9fd844c9ac9ac44a1b703a
Mauro191 <mauro.bia.sistemas@gmail.com>
2024-10-04 22:19:14 -0300

    actividad de la clase 4

--- actividad_clase4.py
+++ actividad_clase4.py
@@ -68,14 +68,25 @@ for numero in lista_numeros:
 
 # Práctica Loop For 3
 # Dada la siguiente lista de números, realiza la suma de todos los números pares e impares* por separado en las variables suma_pares y suma_impares respectivamente:
-suma_pares=list()
-suma_impares=list()
+
+lista_numeros = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+suma_pares=[]
+sumatoria_pares=0
+suma_impares=[]
+sumatoria_impares=0
 for numero in lista_numeros:
-    if numero % 2 == 0:
+    if (numero % 2 == 0):
         suma_pares.append(numero)
     else :
         suma_impares.append(numero)
 
+for numero in suma_pares:
+    sumatoria_pares=sumatoria_pares+numero
+
+for numero in suma_impares:
+    sumatoria_impares=sumatoria_impares+numero
+
+print(sumatoria_pares+sumatoria_impares)
 
 
 # *Recordando de los días anteriores: el módulo (o resto) de un número dividido 2 es cero cuando dicho valor es par, y 1 cuando es impar
@@ -98,7 +109,11 @@ else:
 # Crea un Loop While que se imprima en pantalla los números del 10 al 0, uno a la vez.
 
 numero = 10
-
+while numero != 0 :
+    print(numero)
+    numero=numero-1
+else:
+    print("termino el ciclo")
 
 
 # Práctica Loop While 2
@@ -109,20 +124,22 @@ numero = 10
 # - Si el número no es divisible por 5, continuar ejecutando el loop sin mostrar el valor en pantalla (no te olvides de seguir restando para que el programa no corra infinitamente).
 
 numero = 50
-
-
-
-
+while (numero >= 0):
+    if (numero % 5 == 0):
+        print(numero)
+    numero=numero-1
 
 
 # Práctica Interrupción de Flujo
 # Crea un loop For a lo largo de la siguiente lista de números, imprimiendo en pantalla cada uno de sus elementos, e interrumpe el flujo en el momento que encuentres un valor negativo:
 
 lista_numeros = [4,5,8,7,6,9,8,2,4,5,7,1,9,5,6,-1,-5,6,-6,-4,-3]
+for numero in lista_numeros:
+    print(numero)
+    if numero<=0 : 
+        break
 
 # No debes cambiar el orden de la lista.
-
-
 
 
 for numero in range(0,20,2):
@@ -134,14 +151,24 @@ lista = list(range(0,100))
 # Práctica Rango 1
 # Crea una lista formada por todos los números desde el 2500 al 2585 (inclusive). Almacena dicha lista en la variable mi_lista.
 
+mi_lista=[]
+for numero in range(2500,2586):
+    mi_lista.append(numero)
 
 # Práctica Rango 2
 # Utilizando la función range(), crea en una única linea de código una lista formada por todos los números múltiplos de 3 desde el 3 al 300 (inclusive). Almacena dicha lista en la variable mi_lista.
 
+mi_lista=[]
+for numero in range(3,301,3):mi_lista.append(numero)
 
 
 # Práctica Rango 3
 # Utiliza la función range() y un loop para sumar los cuadrados de todos los números del 1 al 15 (inclusive). Almacena el resultado en una variable llamada suma_cuadrados.
+
+suma_cuadrados=0
+for numero in range(1,16):
+    suma_numeros=numero ** 2
+    suma_cuadrados=suma_cuadrados+suma_numeros
 
 # Para ello:
 
@@ -171,27 +198,32 @@ for indice,item in enumerate(range(50,55)):
 
 # Donde nombre debe ser cada uno de los nombres de la lista a continuación, y el índice, obtenido mediante enumerate().
 
-# lista_nombres = ["Marcos", "Laura", "Mónica", "Javier", "Celina", "Marta", "Darío", "Emiliano", "Melisa"]
-
 # Puedes modificar la línea print() otorgada como ejemplo, pero las frases entregadas deberán ser iguales.
 
 # Tip: utiliza loops!
 
-
-
+lista_nombres = ["Marcos", "Laura", "Mónica", "Javier", "Celina", "Marta", "Darío", "Emiliano", "Melisa"]
+for item in enumerate(lista_nombres):
+    item=item
+for item,nombre in enumerate(lista_nombres):
+    print(f'{nombre} se encuentra en el índice {item}')
 
 # Práctica Enumerador 2
 # Crea una lista formada por las tuplas (indice, elemento), formadas a partir de obtener mediante enumerate() los índices de cada caracter del string "Python".
 
 # Llama a la lista obtenida con el nombre de variable lista_indices .
 
-
+palabra="Python"
+lista_indices=[]
+for indice,caracter in enumerate(palabra) :
+    indices=indice,caracter
+    lista_indices.append(indices)
+    
+print(lista_indices)
 
 
 # Práctica Enumerador 3
 # Imprime en pantalla únicamente los índices de aquellos nombres de la lista a continuación, que empiecen con M:
-
-# lista_nombres = ["Marcos", "Laura", "Mónica", "Javier", "Celina", "Marta", "Darío", "Emiliano", "Melisa"]
 
 # Puedes resolverlo de diferentes maneras, pero servirá que tengas presente todos o algunos de los siguientes elementos:
 
@@ -202,3 +234,12 @@ for indice,item in enumerate(range(50,55)):
 # El método enumerate()
 
 # Métodos de strings o indexado
+
+lista_nombres = ["Marcos", "Laura", "Mónica", "Javier", "Celina", "Marta", "Darío", "Emiliano", "Melisa"]
+
+for indice,nombre in enumerate(lista_nombres):
+    if "M" in nombre :
+        print(f"posicion {indice} , corresponde al nombre {nombre}") 
+    
+    
+         
